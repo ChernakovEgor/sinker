225d0d27ac01ea0ff76a95957cfb65499ff14dd9
Chernakov Egor <chernakov.eg@gmail.com>
2024-09-12 11:03:40 +0300

    add cmd exec

--- main.go
+++ main.go
@@ -3,17 +3,54 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 
-	_ "github.com/pelletier/go-toml/v2"
+	"github.com/pelletier/go-toml/v2"
 	// toml "repo_pusher/pkg/toml_parser"
 )
 
+type Config struct {
+	Remote string
+	Repos  []struct {
+		Name string
+		Path string
+	}
+}
+
 func main() {
-	data, err := os.ReadFile("config.toml")
+	config := ReadConfig("config.toml")
+	PushChanges(&config)
+}
+
+func ReadConfig(path string) Config {
+	file, err := os.Open(path)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
+	defer file.Close()
 
-	fmt.Println(string(data))
+	decoder := toml.NewDecoder(file)
+	config := Config{}
+
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	return config
+}
+
+func PushChanges(config *Config) {
+	for i, repo := range config.Repos {
+		cmd := exec.Command("git", "push", "dev")
+		cmd.Dir = repo.Path
+
+		out, err := cmd.Output()
+		if err != nil {
+			fmt.Fprint(os.Stderr, err)
+		}
+
+		fmt.Printf("%d: %v\n", i+1, string(out))
+	}
 }
