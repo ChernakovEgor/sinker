c435dec6d15b592654d1d9b6d41f0fef4dfb1f0d
Chernakov Egor <chernakov.eg@gmail.com>
2024-09-12 10:36:26 +0300

    gitignore update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module repo_pusher
 
 go 1.22.5
+
+require github.com/pelletier/go-toml/v2 v2.2.3
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
-	toml "repo_pusher/pkg/toml_parser"
+	"os"
+
+	_ "github.com/pelletier/go-toml/v2"
+	// toml "repo_pusher/pkg/toml_parser"
 )
 
 func main() {
-	res := toml.ValidateTOML("")
+	data, err := os.ReadFile("config.toml")
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
 
-	fmt.Println(res)
+	fmt.Println(string(data))
 }
