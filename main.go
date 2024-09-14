87cab35ff1cc1195c36f8cee914c2c0ca5fa2d61
Chernakov Egor <chernakov.eg@gmail.com>
2024-09-15 00:29:46 +0300

    add flags

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	// toml "repo_pusher/pkg/toml_parser"
 )
 
+const usage = `
+usage: rp COMMAND
+
+Commands:
+  pull: pull updates from remotes
+  push: push updates to remotes
+`
+
 type Config struct {
 	Remote string
 	Repos  []struct {
@@ -18,8 +26,24 @@ type Config struct {
 }
 
 func main() {
-	config := ReadConfig("config.toml")
-	PushChanges(&config)
+
+	if len(os.Args) < 2 || (os.Args[1] != "pull" && os.Args[1] != "push") {
+		fmt.Fprint(os.Stderr, usage)
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	switch command {
+	case "pull":
+		config := ReadConfig("config.toml")
+		PullUpdated(&config)
+	case "push":
+		config := ReadConfig("config.toml")
+		PushChanges(&config)
+	default:
+		fmt.Fprint(os.Stderr, usage)
+		os.Exit(1)
+	}
 }
 
 func ReadConfig(path string) Config {
@@ -41,6 +65,20 @@ func ReadConfig(path string) Config {
 	return config
 }
 
+func PullUpdated(config *Config) {
+	for i, repo := range config.Repos {
+		cmd := exec.Command("git", "pull", "--all")
+		cmd.Dir = repo.Path
+
+		out, err := cmd.Output()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%d - %s: %v\n", i+1, repo.Name, err)
+		} else {
+			fmt.Printf("%d - %s: %v\n", i+1, repo.Name, string(out))
+		}
+	}
+}
+
 func PushChanges(config *Config) {
 	for i, repo := range config.Repos {
 		cmd := exec.Command("git", "status", "--porcelain")
@@ -50,7 +88,11 @@ func PushChanges(config *Config) {
 		if err != nil {
 			fmt.Fprint(os.Stderr, err)
 		} else {
-			fmt.Printf("%d - %s: %v\n", i+1, repo.Name, string(out))
+			if len(out) > 0 {
+				fmt.Printf("%d - %s: %v\n", i+1, repo.Name, "uncommited changes")
+			} else {
+				fmt.Printf("%d - %s: %v\n", i+1, repo.Name, string(out))
+			}
 		}
 	}
 }
